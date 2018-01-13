01c289bc9e2171d4a889538922e83041db0390c1
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-01-12 19:41:26 -0600

    Add files via upload
    second part to homework

---
+++ Homework in C/HW1Part2.c
@@ -0,0 +1,21 @@
+/* Kion Smith
+*  kls160430
+*  CS 3377.0W3
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	int c;//current char
+	while ((c = getchar()) != EOF)// while char is not end of file
+	putchar(c);// place char to conolse or file(specified as ./a.out < file >  other file)
+	
+
+
+}
+
+
+
+
