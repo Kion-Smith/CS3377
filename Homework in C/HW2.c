aeaea91b7bd1c59afb6736450cfb1e1b58bae078
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-01-24 01:33:42 -0600

    Working homework 2

--- Homework in C/HW2.c
+++ Homework in C/HW2.c
@@ -1,41 +1,68 @@
+/* Kion Smith
+*  kls160430
+*  CS 3377.0W3
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char*argv[])
 {
+	//the file pointer and 
+	//char to loop through the file with
+	//counter to hold how many characters where counted
 	FILE *fp;
-	fp = fopen( argv[1], "r" );
 	char c;
 	int count=0;
 
+	//open the file(uses the console to run the file)
+	// ex ./a.out file.c
+	fp = fopen( argv[1], "r" );
+	
+	//if the file is null, print that the fly could not be found
 	if(fp == NULL)
 	{
 		printf("The file could not be found\n");
 	}
 	else
-	{
+	{	//while next is not end of file
 		while ( (c = fgetc( fp )) != EOF )
-		{/*
-			if(c == '/')
-			{
-				if((c = fgetc(fp)) == '/')
-				{
-					count++;
+		{	//if c is a single slash
+			if(c =='/')
+			{	//and if the next item is also a slash(//)
+				if( (c = fgetc(fp)) == '/')
+				{	//loop until the end of line character
+					while((c = fgetc(fp)) != '\n')
+					{	
+						count++;
+					}
 				}
-
-			}
-			*/
-
-			switch( c )
-			{
-				case '/':
+				//if the next item is a * (/*)
+				else if(c == '*')
+				{	//if the end could / is found loop until end
+					if((c = fgetc(fp)) == '/')
+					{
+						while((c = fgetc(fp)) != '\n')
+						{
+								count++;
+						}
+					}
+					// else the count the current characters until /
+					else
+					{
+						while((c = fgetc(fp)) != '/')
+						{
+								count++;
+						}
+					}	
 					
-			 	case '\n' : count++; 
-			  	default   : break;
-			}
-		}
 
-		printf("%d \n", count);
+				}
+			}
+
+		}
+		//out put the results
+		printf("Character spaces counted = %d \n", count);
 	}
 
 	
