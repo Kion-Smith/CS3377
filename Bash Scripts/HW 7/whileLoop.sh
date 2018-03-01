298fa22266f4c9c2aebca2a6c71c0ad4e294a502
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-03-01 17:10:19 -0600

    added the loop scripts

---
+++ Bash Scripts/HW 7/whileLoop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Enter a file name to out put the random nums"
+read filename
+count=1
+while [ "$count" -le 100 ]
+do
+randomNum=$[RANDOM%100]
+echo "$randomNum" >> $filename
+count=$[count+1]
+done
+echo "Finished printing to" $filename
