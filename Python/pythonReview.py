8f4e6460f8c882e24d6d7a4f1f03fea24c5de10d
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-04-28 00:16:27 -0500

    Finished this review problem

--- Python/pythonReview.py
+++ Python/pythonReview.py
@@ -1,19 +1,19 @@
 #Kion Smith
 #REVIEW FOR FINAL
 import random
-
+#Global Variables
 randArray = []
 posNum = int(0)
 negNum = int(0)
-
 minNum = int()
 maxNum = int()
-
 sumOfNums = int()
 
+#function to create random num
 def createNumber(min,max):
     return( (random.randint(min,max)) )
 
+#get the sign of a number
 def countSigns(num):
     if num>-1:
         global posNum
@@ -21,8 +21,8 @@ def countSigns(num):
     else:
         global negNum
         negNum += 1
-        
-#could also just search array for min and max
+#looking for min an max        
+#could also just search array for min and max, would be better to just check during running
 def getMin(cur):
     global minNum
     if cur<minNum:
@@ -32,8 +32,30 @@ def getMax(cur):
     if cur>maxNum:
         maxNum = cur
         
+#opening reports and writing everything to a file       
+def fileWrite(aoo):
+    f = open("Reports.txt","a")
+    f.write("Postives = ")
+    f.write(str(posNum)) #converts to a string (need to do this)
+    f.write("\n")
+    f.write("Negatives = ")
+    f.write(str(negNum))
+    f.write("\n")
+    f.write("Min = ")
+    f.write(str(minNum))
+    f.write("\n")
+    f.write("Max = ")
+    f.write(str(maxNum))
+    f.write("\n")
+    f.write("Avg = ")
+    f.write(str(avg))
+    f.write("\n")
+    f.write("------------------------ \n")
+    f.close()
     
+#main loop   
 count = 1;
+#loop for 1-100
 while count<=100:
     curNum = createNumber(-25,25)
     countSigns(curNum)
@@ -50,3 +72,4 @@ print("Postive",posNum,"Negatives",negNum)
 print("Min",minNum,"Max",maxNum)
 print("Sum",sumOfNums)
 print("Avg",avg)
+fileWrite(avg)
